0e27c83d8666e387ea4106dc1394064b344f8cca
Kyrylo Silin <silin@kyrylo.org>
2024-06-05 19:30:49 +0300

    Make it possible to configure api_key

--- lib/telebugs.rb
+++ lib/telebugs.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 require_relative "telebugs/version"
+require_relative "telebugs/config"
 
 module Telebugs
-  class Error < StandardError; end
-  # Your code goes here...
+  # The general error that this library uses when it wants to raise.
+  Error = Class.new(StandardError)
+
+  class << self
+    def configure
+      yield Telebugs::Config.instance
+    end
+  end
 end
--- test/test_telebugs.rb
+++ test/test_telebugs.rb
@@ -6,4 +6,14 @@ class TestTelebugs < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Telebugs::VERSION
   end
+
+  def test_configure_configures_project_key
+    key = "12345:abcdef"
+
+    Telebugs.configure do |config|
+      config.api_key = key
+    end
+
+    assert_equal key, Telebugs::Config.instance.api_key
+  end
 end
