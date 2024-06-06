52b6a024fda25885a81165ef6c4d6643bfa1f879
Kyrylo Silin <silin@kyrylo.org>
2024-06-06 18:05:47 +0300

    Add the WrappedError class

--- lib/telebugs.rb
+++ lib/telebugs.rb
@@ -5,6 +5,7 @@ require "concurrent"
 require_relative "telebugs/version"
 require_relative "telebugs/config"
 require_relative "telebugs/promise"
+require_relative "telebugs/wrapped_error"
 
 module Telebugs
   # The general error that this library uses when it wants to raise.
